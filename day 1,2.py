9cef98a93f7faa659e44b8c1ff15ea9c993ede64
totoglitch <aborastarek90@gmail.com>
2025-02-09 16:33:16 +0200

    Add files via upload

---
+++ day 1,2.py
@@ -0,0 +1,100 @@
+#day 1 :ex 1:Using this class
+#class Cat: def __init__(self, cat_name, cat_age):
+# self.name = cat_name
+#  self.age = cat_age
+# Instantiate three Cat objects using the code provided above.   
+# Outside of the class, create a function that finds the oldest cat and returns the cat.    
+#Print the following string: “The oldest cat is <cat_name>, and is <cat_age> years old.”. Use the function previously created.
+class Cat:
+     def __init__(self, cat_name, cat_age,cat_color): self.name = cat_name,self.age=cat_age, self.color=cat_color
+     def meo(self):return f"{self.cat_name})make meo sound"
+def play_ball(self):return f"{self.cat_color}play with same ball color"
+print("a new cat has been initialized")
+def find_oldest_cat(self):self.age>"cat_age"
+if"{ age>cat_age}":print('cat_name')
+print("the oldest cat is{cat_name}and is{cat_age}years old")
+#ex 2 :Instructions:Create a class called Dog.
+#In this class, create an __init__ method that takes two parameters : name and height.
+ #This function instantiates two attributes, which values are the parameters.
+#Create a method called bark that prints the following string “<dog_name> goes woof!”.
+#Create a method called jump that prints the following string “<dog_name> jumps <x> cm high!”. x is the height*2.
+#Outside of the class, create an object called davids_dog. His dog’s name is “Rex” and his height is 50cm.
+#Print the details of his dog (ie. name and height) and call the methods bark and jump.
+#Create an object called sarahs_dog. Her dog’s name is “Teacup” and his height is 20cm.
+#Print the details of her dog (ie. name and height) and call the methods bark and jump.
+#Create an if statement outside of the class to check which dog is bigger. Print the name of the bigger dog.
+class dog:
+     def _init_(self,name,height_):print(name)
+     def bark(self):return f"{self.name}goes woof !"
+     x="{self.height}*2"
+     def jump(self):return f"{self.name}jumps <x> cm high!"
+     davids_dog='dog'('rex','50cm')
+     print(davids_dog)
+     def bark(davids_dog):return "{davids_dog}goes woof!"
+     def jump(davids_dog):return "{davids_dog}jumps <x> cm high!"
+     sarahs_dog=['teacup','20cm']
+     print(sarahs_dog)
+     def bark(sarahs_dog):return "{sarahs_dog} goes woof!"
+     def jump(sarahs_dog):return "{sarahs_dog}jumps<x>cm high!"
+     if (davids_dog)>(sarahs_dog):print('rex')
+     #ex 3:Instructions:Define a class called Song, it will show the lyrics of a song.
+#Its __init__() method should have two arguments: self and lyrics (a list).
+#Inside your class create a method called sing_me_a_song that prints each element of lyrics on its own line.
+#Create an object, for example:stairway= Song(["There’s a lady who's sure","all that glitters is gold", "and she’s buying a stairway to heaven"])
+#Then, call the sing_me_a_song method. The output should be:There’s a lady who's sure all that glitters is gold
+#and she’s buying a stai
+class song:
+     def _init_(self,lyrics):self.lyrics=["Theres a lady who's sure","all that glitters is gold"]
+def sing_me_a_song(self):
+     print(self.lyrics)
+     for each_lyric in self.lyrics:print(each_lyric)
+     if ('stairway')==song(["There’s a lady who's sure","all that glitters is gold", "and she’s buying a stairway to heaven"]):
+          print("There’s a lady who's sure all that glitters is gold and she’s buying  a stairway to heaven")
+          #ex 4 :Instructions:Create a class called Zoo.In this class create a method __init__ that takes one parameter: zoo_name.
+#It instantiates two attributes: animals (an empty list) and name (name of the zoo).
+#Create a method called add_animal that takes one parameter new_animal. This method adds the new_animal to the animals list as long as
+# it isn’t already in the list.
+#Create a method called get_animals that prints all the animals of the zoo.
+#Create a method called sell_animal that takes one parameter animal_sold. This method removes the animal from the list and of course
+# the animal needs to exist in the list.
+#Create a method called sort_animals that sorts the animals alphabetically and groups them together based on their first letter.
+#Example:: "Ape",
+  #  B: ["Baboon", "Bear"],
+  #  C: ['Cat', 'Cougar'],
+  #  E: ['Eel', 'Emu']
+#Create a method called get_groups that prints the animal/animals inside each group.
+#Create an object called ramat_gan_safari and call all the methods.
+#Tip: The zookeeper is the one who will use this class.
+#Example:Which animal should we add to the zoo -->  girafe x.add_animal(
+class zoo:
+     def __init__(self,zoo_name,animals_name):print(zoo_name,animals_name)
+def add_animals(self,new_animal):print(new_animal)
+def get_animals(self,new_animal,animals_name):print(get_animals)
+def sell_animal(self,animal_sold): get_animals>1,print('no_proplem')
+if get_animals<1:print(f'its_sold_out')
+def sort_animal(self,a,b,c,z):print({a},{b},{c},{z})
+ramat_gan=get_animals+sort_animal-sell_animal
+
+
+          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
